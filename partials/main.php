b381a1fd3f13e57066e98d9937aee381e2b0f806
Noemi <noemi.rea@libero.it>
2021-04-27 17:38:23 +0200

    esercizio 1

--- partials/main.php
+++ partials/main.php
@@ -1,18 +1,17 @@
 <main>
     <div class="container">
-        <?php 
-            foreach($database as $album) {
-                echo '<div class="album_container" > 
-                        <div class="album_image">
-                            <img src="'.$album["poster"].'">
-                        </div>
-                        <div class="album_title">'.
-                            '<p>'.$album["title"].'</p>'.
-                            '<p>'.$album["author"].'</p>'.
-                            '<p>'.$album["year"].'</p>'.
-                        '</div>'. 
-                    '</div>';        
-            }
-        ?>
+        <?php  foreach($database as $album) { ?>
+            <div class="album_container" > 
+                <div class="album_image">
+                    <?php echo '<img src="'.$album['poster'].'">'; ?>
+                </div>
+                <div class="album_title">
+                     <?php  echo '<p>'.$album['title'].'</p>'; 
+                            echo '<p>'.$album['author'].'</p>'; 
+                            echo '<p>'.$album['year'].'</p>'; 
+                    ?>        
+                </div> 
+            </div>        
+        <?php } ?>
     </div>
 </main>
